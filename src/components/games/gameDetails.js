8786c6d3fe3ed891b2463115bdc15bd7c7ec285b
Josip Papez <papezjosip@gmail.com>
2019-12-10 22:12:39 +0100

    added videos in detailed description

--- src/components/games/gameDetails.js
+++ src/components/games/gameDetails.js
@@ -3,15 +3,37 @@ import { connect } from "react-redux";
 import { fetchGameDetail } from "../../store/actions/gamesActions";
 
 class GameDetails extends Component {
-    componentWillMount(){
+    componentDidMount(){
         this.props.fetch(this.props.match.params.id);
+    }
+    extract(propertyName, object) {
+        const parts = propertyName.split(".");
+        let length = parts.length;
+        let i;
+        let property = object || this;
+
+        for (i = 0; i < length; i++) {
+            property = property[parts[i]];
+        }
+
+        return property;
     }
     render(){
         let { game } = this.props;
+        let preview = this.extract('preview', game['clip']);
+        let clip = this.extract('clip', game['clip']);
+        console.log(preview);
+        console.log(clip);
+        
         return (
             <div className="detail-card grid-container">
-                <div className="detail-card-image hoverable">
-                    <img src={game.background_image} alt={game.slug}/>
+                <div>
+                    <div className="detail-card-image hoverable">
+                        <img src={game.background_image} alt={game.slug}/>
+                    </div>
+                    <div className="detail-video-clip">
+                        <video controls src={clip} muted/>
+                    </div>
                 </div>
                 <div className="detail-card-content">
                     <p className="detail-card-title"><b>{game.name}</b></p>
